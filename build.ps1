bef529d8fe733acf4864664514835c8ebb3a5604
Michael Carlson <Michael.Carlson2@extendhealth.com>
2018-04-18 15:15:03 -0600

    move versioning to zip instead of publish call

--- build.ps1
+++ build.ps1
@@ -23,22 +23,22 @@ if(Test-Path $publish) {Remove-Item $publish -Force -Recurse}
 
 if(Test-Path "..\..\artifacts") {RemoveItem "..\..\artifacts" -Force -Recurse}
 
-Write-Host ""
-Write-Host "Building solution projects...."
+Write-Host "";
+Write-Host "Building solution projects....";
 exec { & dotnet build .\src\Walawren.Grappnel.Website.sln -c Release }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-Write-Host ""
-Write-Host "Running solution tests...."
+Write-Host "";
+Write-Host "Running solution tests....";
 exec { & dotnet test .\src\Walawren.Grappnel.Website.Tests -c Release }
 
-Write-Host ""
-Write-Host "Publishing and zipping artifacts...."
-exec { & dotnet publish .\src\Walawren.Grappnel.Website -c Release -o $publish --version-suffix=$revision /p:PackageAsSingleFile=true }
+Write-Host "";
+Write-Host "Publishing and zipping artifacts....";
+exec { & dotnet publish .\src\Walawren.Grappnel.Website -c Release -o $publish /p:PackageAsSingleFile=true }
 $source = "$(Get-ScriptDirectory)\publish\*"
 $destinationPath = "$(Get-ScriptDirectory)\artifacts\"
 New-Item -ItemType Directory -Force -Path $destinationPath
-$destination = "$($destinationPath)Walawren.Grappnel.Website.zip"
+$destination = "$($destinationPath)Walawren.Grappnel.Website.0.0.0.$($revision).zip"
 Compress-Archive -Path $source -DestinationPath $destination
